336dd588ba448fe190f638d1855077656ff60aa3
TheLim148 <ruzhov34@gmail.com>
2025-06-14 15:29:09 +0300

    Added RandomStyle func. Not ideal. Maybe I'll finish it later

--- main.cpp
+++ main.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
 #include <limits>
 #include <string>
+#include <random>
+#include <cctype>
 
 using namespace std;
+
+bool isUpperCase(const string& str) {
+    for (char c : str) {
+        if (!isupper(static_cast<unsigned char>(c))) {
+        return false;
+        }
+    }
+    return true;
+}
+
+void RandomStyle() {
+    string str;
+    bool upper = true;
+    cout << "Enter your string: ";
+    getline(cin, str);
+
+    default_random_engine generator;
+    uniform_int_distribution<int> distribution(0, str.length());
+
+    for (char &ch : str) {
+        if (isalpha(ch)) {
+            int num = distribution(generator);
+            if (num > str.length() / 2) {
+                ch = upper ? toupper(ch) : tolower(ch);
+            }
+        }
+    }
+    cout << "Returned string: " << str << endl;
+}
 
 void ZaborStyle() {
     string str;
@@ -42,19 +73,17 @@ int main() {
     cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
     switch(n) {
-        case 1:
-            ZaborStyle();
-            break;
-        case 2:
-            cout << "There is no function";
-            break;
-        case 3:
-            cout << "Goodbye!";
-            break;
-        default:
-            cout << "Man.";
-            break;
-
+    case 1:
+        ZaborStyle();
+        break;
+    case 2:
+        RandomStyle();
+        break;
+    case 3:
+        cout << "Goodbye!";
+        break;
+    default:
+        cout << "Man.";
     }
 
     return 0;
